04de1717be8d647d3606a78c0f3d8521f0aad92c
Haoxuan Guo <guo.haoxuan@outlook.com>
2024-02-28 20:24:19 +0800

    fix: fix plot extension

--- ext/SpaceSplitMakieExt.jl
+++ ext/SpaceSplitMakieExt.jl
@@ -2,9 +2,10 @@ module SpaceSplitMakieExt
 
 using SpaceSplit
 using Meshes
+using Makie
 
 function Meshes.viz(bvh::BVH)
-    f = fuse(bvh)
+    f = SpaceSplit.fuse(bvh)
     for i in 1:length(f)
         if i == 1
             viz(f[i]; showfacets=true, alpha=0, segmentsize=2)
